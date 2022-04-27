a2feb36d8e30f0e38334c33150784968c92f0e63
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-04-27 17:04:55 -0300

    abilitando cors no php

--- Backend/Pages/index.php
+++ Backend/Pages/index.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
 //api/Nobreaks/1
 
 require_once '../vendor/autoload.php';
--- Frontend/src/js/script.js
+++ Frontend/src/js/script.js
@@ -1,1 +1,16 @@
 // http://localhost/sistema_controle_ti/Backend/Pages/api/Nobreak/1
+
+const url = 'http://localhost/sistema_controle_ti/Backend/Pages/api/Nobreak/'
+
+function getNobreaks() {
+  axios
+    .get(url)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+getNobreaks()
