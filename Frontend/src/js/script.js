ce0685f8c1e7514989c6cd5c52672eabede785bd
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-04-29 14:51:55 -0300

    criado a tela de listagem dos nobreaks

--- Frontend/src/js/script.js
+++ Frontend/src/js/script.js
@@ -2,11 +2,15 @@
 
 const url = 'http://localhost/sistema_controle_ti/Backend/Pages/api/Nobreak/'
 
+let data = null
+
 function getNobreaks() {
   axios
     .get(url)
     .then(response => {
-      console.log(response)
+      data = response.data.data
+
+      makeInterface(data)
     })
     .catch(error => {
       console.log(error)
@@ -14,3 +18,40 @@ function getNobreaks() {
 }
 
 getNobreaks()
+
+function makeInterface(data) {
+  const table = document.getElementById('nobreaks')
+
+  data.map(data => {
+    table.innerHTML += `
+      <tr class='table-dark'>
+        <td>${data.id}</td>
+        <td>${data.patrimonio ? data.patrimonio : ''}</td>
+        <td>${data.marca}</td>
+        <td>${data.modelo}</td>
+        <td>${data.local_atual}</td>
+        <td>${data.responsavel}</td>
+        <td>
+          <button onClick={editItem(${
+            data.id
+          })} type="button" class="btn btn-secondary btn-sm">Editar</button>
+          <button onClick={deleteItem(${
+            data.id
+          })} type="button" class="btn btn-outline-danger btn-sm">Excluir</button>
+        </td>
+      </tr>
+    `
+  })
+}
+
+function editItem(id) {
+  console.log('Clicado em Editar como id: ' + id)
+}
+
+function deleteItem(id) {
+  console.log('Clicado em Deletar como id: ' + id)
+}
+
+function createItem() {
+  console.log('Clicado em Criar')
+}
